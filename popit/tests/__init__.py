9e49126f35785ed62ed2333b0aed5215b387482f
Edmund von der Burg <evdb@mysociety.org>
2013-03-28 08:12:10 +0000

    Setup testing

---
+++ popit/tests/__init__.py
@@ -0,0 +1,1 @@
+from .popit_instance import *
---
+++ popit/tests/test_settings.py
@@ -0,0 +1,31 @@
+"""Settings that need to be set in order to run the tests."""
+import os
+
+
+DEBUG = True
+USE_TZ = True
+SITE_ID = 1
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME":   "popit-django",
+    }
+}
+
+ROOT_URLCONF = 'popit.tests.urls'
+
+CURRENT_DIR = os.path.dirname(__file__)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(CURRENT_DIR, '../../static/')
+STATICFILES_DIRS = (
+    os.path.join(CURRENT_DIR, 'test_static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(CURRENT_DIR, '../templates'),
+)
+
+INSTALLED_APPS = [
+    'popit',
+]
